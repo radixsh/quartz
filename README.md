1a3735689b9deb7f5efdca34e708c09bb301dfae
radradix <53844660+radradix@users.noreply.github.com>
2021-06-19 14:17:59 -0700

    TIL that you're supposed to put spaces between if and the statement you're checking

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# bitz — discord bot 
-bitz is a [node.js](https://nodejs.org/en/) discord bot who can echo messages, uwuify messages, delete messages, and create polls. their most distinctive feature is that they respond to messages containing the strings `owo` and `uwu`. 
+# bitz — discord bot
+bitz is a [node.js](https://nodejs.org/en/) discord bot who can echo messages, uwuify messages, delete messages, and create polls. their most distinctive feature is that they respond to messages containing the strings `owo` and `uwu`.
 
-bitz was named by my close friend tumega500#4689; their profile picture art was also created by him. 
+bitz was named by my close friend tumega500#4689; their profile picture art was also created by him.
 
 ## table of contents  
 1. [general info](#general-info)
@@ -13,96 +13,61 @@ bitz was named by my close friend tumega500#4689; their profile picture art was 
 7. [license](#license)
 
 ## general info
-i created bitz for three reasons: 
-* to learn a bit of javascript to complement my basic understanding of html and css, 
-* to apply what i learned in apcsa in junior year, and 
-* to have fun creating a bot for friends in the summer. 
+i created bitz for three reasons:
+* to learn a bit of javascript to complement my basic understanding of html and css,
+* to apply what i learned in apcsa in junior year, and
+* to have fun creating a bot for friends in the summer.
 
 i could've chosen to learn python and use that to write bitz, but to be honest it was hard enough figuring out the discord api already, as well as fun enough learning how to use the discord.js documentation (and stack overflow wisdom!) to do what i wanted to do.
 
 ## features
 * convenient mass-deletion of recent messages (honestly, that feature is missing from discord)
-* poll creation (another feature missing from discord) 
-* anyone can `$echo` anything, which is a good feature for pseudo-anonymity and general shenanigans 
+* poll creation (another feature missing from discord)
+* anyone can `$echo` anything, which is a good feature for pseudo-anonymity and general shenanigans
 * bonus: will train you to use uwuspeak more often
 
 currently available commands (`$command <argument> [optional argument]`):
 
-* `$h[elp]` : shows this help page
-* `$purge <n>` : deletes the `n` most recent messages in the current channel (2 < `n` < 100), and also deletes the command message
-* `$echo <foo>` : echoes back what you tell it to, deleting the command message. (it works for one image at a time too)
-* `$poll "<polling question>" "<poll answer 1>" "<poll answer 2"> "[poll answer 3]" ...` : creates a poll in an embed, deleting the command message. at least three arguments are necessary, set off by double quotation marks: a question and at least two options.
-  + here's what a poll might look like: 
-  + $[poll](https://i.imgur.com/GHtRUHem.png)
-* `$ping` : performs a ping
-* `$uwu` : uwuifies your messages
+* `$purge num` : deletes the `num` most recent messages in the current channel (2 < `num` < 100), and also deletes the command message
+* `$echo foo` : echoes back what you tell it to, deleting the command message.
+* `$poll "foo?" "bar" "bar but not" "[bar but better]" ...` : creates a poll in an embed, deleting the command message. at least three arguments are necessary, set off by double quotation marks: a question and at least two options.
+* `$uwu bar` : uwuifies your messages (turning `bar` into `baw`)
 * `$uwuchannel [-rm]` : uwuifies all future messages in the current channel. the option `-rm` removes this setting.
-* `$data` : gets metadata of the current guild and channel, and also gives information about you.
-
-
-here's some sample console output :)
-![sample console output](https://i.imgur.com/aVTbNcQ.png)
-
+* `$cc[d]` : counts characters in your message (ignoring the command's length). use `ccd` instead to delete the message.
+* `$wc[d]` counts words in your message separated by spaces and em dashes (ignoring the command's length). use `wcd` instead to delete the message.
+* `$data` : gets data about the current guild, current channel, and you :)
 
 ## technologies
-this project was created with: 
+this project was created with:
 * [node.js](https://nodejs.org/en/) v12.18.0: an open-source, cross-platform javascript runtime environment that executes javascript code outside a web browser
 * [discord.js](https://discord.js.org/#/) v12.2.0 ([documentation](https://discordjs-fork.readthedocs.io/en/latest/index.html)): a powerful object-oriented node.js module that allows easy interaction with the discord api
-* [discord api](https://discord.com/developers/docs/intro) 
+* [discord api](https://discord.com/developers/docs/intro)
 * [nodemon](https://www.npmjs.com/package/nodemon) v2.0.4: a tool that helps develop node.js based applications by automatically restarting the node application when file changes in the directory are detected
 
 ## setup
-to clone and run this application, you'll need git and node.js (which comes with npm).
+to clone and run this application, you'll need git and node.js (which comes with npm). clone it with `git clone https://github.com/radradix/bitz`. before starting bitz, you'll need to make your own config.json file, which will look something like this:
 
-on mac (i did this): 
-```sh
-#you'll need to have homebrew and git first
-#here's the installation command for homebrew from https://brew.sh: 
-$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-#install git
-$ brew install git
-
-#install node.js using homebrew
-$ brew install node
-
-#install discord.js
-$ npm install discord.js opusscript --save
-
-#clone this repository
-$ git clone https://github.com/radradix/bitz
-
-#go into the newly created repository
-$ cd bitz
-```
-
-before starting bitz, you'll need to make your own config.json file:
-
-```sh
-$ rm config.json
-$ touch config.json
-```
-
-the config file will hold, at minimum, your token and your chosen command prefix (in the command `$help`, for instance, the prefix is `$`). to generate your own api token, go to your [discord developer portal](https://discord.com/developers/applications) and create an application. click the subheading "bot" in the menubar on the left and add a bot, and a token will have been generated under the bot's username. (this token must be kept secret.) here's the format that i used: 
 ```js
 {
-  "token": "my-token-here-between-quotes",
-  "prefix": "$",
-  "permittedGuilds": "1234567890"
-  "inferkitToken": "asdkfjaksdjf-ksdjfkdjf"
+     "token": "your-token-here",
+     "prefix": "$",
+     "permittedGuilds": ["1234567890"]
 }
 ```
+
+to generate an api token, go to the [discord developer portal](https://discord.com/developers/applications) and create an application. click the subheading "bot" in the menubar on the left and add a bot, and a secret token will have been generated under the bot's username.
+
 (the permittedGuilds line allows you the option to silence bitz everywhere except the sandbox guild, for instance while testing beta features. you can add whatever constants you want to the config file; just make sure the last line doesn't end with a trailing comma.)
 
-once your bot's been made, you'll need to add it to a discord guild (colloquially, "server") in which you have admin privileges. to do that, go to your developer portal again and create a bot invite link with the permissions integer `3209216`. 
+once your bot's been made, you'll need to add it to a discord guild (colloquially, "server") in which you have admin privileges. to do that, go to your developer portal again and create a bot invite link with the permissions integer `10304`.
 
 finally, run `node index.js` (or `nodemon`) to start bitz. then you're finished!
 
 ## project status
-bitz is phasing out of development since i realized that time-based tasks are much better supported in discord.ext.tasks (an extension to discord.py, rather than discord.js). 
+bitz is phasing out of development since i realized that time-based tasks are much better supported in discord.ext.tasks (an extension to discord.py, rather than discord.js).
 
 ## sources
-to create this project, i found the following sources incredibly useful: 
+to create this project, i found the following sources incredibly useful:
 * [*discord.js guide*](https://discordjs.guide/) — an invaluable source of explanations and examples! second only to official documentation! specifically, i used:
   + [getting user input](https://discordjs.guide/creating-your-bot/commands-with-user-input.html#basic-arguments)
   + [adding more commands](https://discordjs.guide/creating-your-bot/adding-more-commands.html)
@@ -112,7 +77,7 @@ to create this project, i found the following sources incredibly useful:
   + [miscellaneous examples](https://discordjs.guide/popular-topics/miscellaneous-examples.html#play-music-from-youtube), including playing music from youtube and retrieving emoji characters from another file created in the same directory as index.js  
 * [how to create a music bot (*free code camp*)](https://www.freecodecamp.org/news/how-to-create-a-music-bot-using-discord-js-4436f5f3f0f8/)
 * [*an idiot's guide*](https://anidiots.guide/)
-  + [adding a config file](https://anidiots.guide/first-bot/adding-a-config-file) – this was helpful for conceptualizing the flow of order when the bot is running under `nodemon`. using a config file requires understanding of how the config file responds to calls from the index file, as well as comprehension of the syntax necessary to do so. this blew my mind. 
+  + [adding a config file](https://anidiots.guide/first-bot/adding-a-config-file) – this was helpful for conceptualizing the flow of order when the bot is running under `nodemon`. using a config file requires understanding of how the config file responds to calls from the index file, as well as comprehension of the syntax necessary to do so. this blew my mind.
   + [understanding roles and permissions](https://anidiots.guide/understanding/roles)
 
 ## license
--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const { token, prefix, permittedGuilds, inferkitToken} = require("./config.json");   
+const { token, prefix, permittedGuilds, inferkitToken} = require("./config.json");
 const emojiCharacters = require("./emojiCharacters.js");
 const fs = require('fs');
 var exec = require('child_process').exec;
 var isDm = false;
-
 require('dotenv').config();
-//const InferKit = require("./inferkitIndex.js")
 
 bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`); 
+    console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', async message => {
     if (message.author.bot) return;
-    
-    //uncomment this line if you want to silence bitz everywhere except the permitted guilds: 
+
+    //uncomment this line if you want to silence bitz everywhere except the permitted guilds:
     //if(!permittedGuilds.includes(message.guild.id)) return;
-    
+
     fs.readFile('uwuchannels.txt', 'utf8', function(err, uwuchannels) {
-        if (message.content.substring(0,prefix.length) !== prefix && 
-            !message.content.toLowerCase().includes('uwu') && 
+        if (message.content.substring(0,prefix.length) !== prefix &&
+            !message.content.toLowerCase().includes('uwu') &&
             !message.content.toLowerCase().includes('owo') &&
             !message.content.toLowerCase().includes('μωμ') &&
             !uwuchannels.includes(message.channel.id))
-        return; 
+        return;
     });
-    
-    if (message.content === `${prefix}uwuchannel -rm`){
+
+    if (message.content === `${prefix}uwuchannel -rm`) {
         fs.readFile('uwuchannels.txt', 'utf8', function(err, uwuchannels) {
-            if (!uwuchannels.includes(message.channel.id)){
+            if (!uwuchannels.includes(message.channel.id)) {
                 return message.channel.send("it wasn't already set, so nothing's been done");
             }
             exec(`sed -i "" /${message.channel.id}/d uwuchannels.txt`, function (error) {
@@ -38,12 +36,12 @@ bot.on('message', async message => {
             });
             return message.channel.send("uwuchannel removed!");
         });
-    } 
+    }
 
     // if it starts with the prefix, then separate the message into the command (first term) and the arguments
     const punctuationArray = ["?","!",".","~"];
     if (message.content.substring(0,prefix.length) === prefix) {
-        var args = message.content.slice(prefix.length).trim().split(/\s+/g); 
+        var args = message.content.slice(prefix.length).trim().split(/\s+/g);
         var command = args.shift().toLowerCase();
         var len = 0;
         for (let i = 0; i < command.length; i++) {
@@ -53,12 +51,12 @@ bot.on('message', async message => {
         }
         if (len > 0 || len === command.length) return;
         if (!message.channel.name) isDm = true;
-        
+
         if (command === 'ping' || command === 'p') {
             const m = await message.channel.send("ping?");
             return m.edit(`pong! latency is ${m.createdTimestamp - message.createdTimestamp} ms :)`);
-        } 
-        
+        }
+
         else if (command === "help" || command === "h") {
             const helpEmbed = new Discord.MessageEmbed()
                 .setColor('#8db255')
@@ -75,18 +73,17 @@ bot.on('message', async message => {
                     { name: `\`${prefix}uwuchannel [-rm]\``, value: "uwuifies all future messages in the current channel. the option `-rm` removes this setting."},
                     { name: `\`${prefix}cc[d]\``, value: "counts characters in your message (ignoring the command's length). use `ccd` instead to delete the message."},
                     { name: `\`${prefix}wc[d]\``, value: "counts words in your message (ignoring the command's length). use `wcd` instead to delete the message." },
-                    //{ name: `\`${prefix}ai foo bar\``, value: "lets you talk with inferkit's api."},
                     { name: `\`${prefix}data\``, value: "gets data about the current guild, current channel, and you :)"}
                 )
                 .setFooter('developed by radix#4520');
-            return message.channel.send(helpEmbed);    
-        } 
-        
+            return message.channel.send(helpEmbed);
+        }
+
         else if (command === "echo") {
             var textToEcho = args.join(" ");
-            if (args.length === 0) 
+            if (args.length === 0)
                 return message.channel.send("bruh");
-            if (message.attachments.size === 0) 
+            if (message.attachments.size === 0)
                 message.channel.send(textToEcho);
             else {
                 try {
@@ -102,86 +99,86 @@ bot.on('message', async message => {
                     console.log(error)
                 }
             }
-            return message.delete().catch(O_o=>{}); 
-        } 
-        
+            return message.delete().catch(O_o=>{});
+        }
+
         else if(command === "purge") {
             if (isDm) return message.channel.send("(i'm not allowed to delete things in dms :/)");
             const deleteCount = parseInt(args[0], 10);
             if (!deleteCount || deleteCount < 2 || deleteCount > 99)
                 return message.channel.send("you're supposed to provide a number between 2 and 99 for the number of messages to delete :/");
-            try { 
-                message.channel.bulkDelete(deleteCount+1); 
+            try {
+                message.channel.bulkDelete(deleteCount+1);
                 return message.channel.send(`successfully purged ${deleteCount} messages!`)
             }
-            catch (error) { 
-                return message.channel.send(`couldn't delete because: ${error}`); 
-            } 
-        } 
-        
+            catch (error) {
+                return message.channel.send(`couldn't delete because: ${error}`);
+            }
+        }
+
         else if (command === "poll") {
-            if (args[0].substring(0,1) !== "\"")
+            if (args[0].substring(0,1) !== "\"") {
                 return message.channel.send(`you might wanna check the ${prefix}help page again for the poll syntax :/`);
+            }
             var poll = args.join(" ");
             var pollQuestion = poll.substring(1,poll.indexOf("\"",1));
 
             var howManyOptions = 0;
-            for (let i = 0; i < poll.length; i++){
-                if (poll[i] === "\"")
-                    howManyOptions++;
+            for (let i = 0; i < poll.length; i++) {
+                if (poll[i] === "\"") howManyOptions++;
             }
             howManyOptions = Number(howManyOptions/2-1);
             if (howManyOptions - Math.floor(howManyOptions) != 0)
                 return message.channel.send("errr,,,, think you made a mistake :/");
-            if (howManyOptions < 2 || howManyOptions > 10) 
+            if (howManyOptions < 2 || howManyOptions > 10)
                 return message.channel.send("you're supposed to provide between 2 and 10 (inclusive) poll options :/");
-            
-            var pollOptions = []; 
+
+            var pollOptions = [];
             var nextQuoteIndex = poll.indexOf("\"",poll.indexOf("\"",1));
             nextQuoteIndex = poll.indexOf("\"",nextQuoteIndex+1);
-            for(let i = 0; i < howManyOptions; i++){
-                if(nextQuoteIndex !== -1){
+            for (let i = 0; i < howManyOptions; i++) {
+                if (nextQuoteIndex !== -1) {
                     pollOptions[i] = poll.substring(nextQuoteIndex+1,poll.indexOf("\"",nextQuoteIndex+1));
                     nextQuoteIndex = poll.indexOf("\"",nextQuoteIndex+1);
                     nextQuoteIndex = poll.indexOf("\"",nextQuoteIndex+1);
                 }
             }
-            message.delete().catch(O_o=>{}); 
+            message.delete().catch(O_o=>{});
             var options = "";
-            for(let i = 0; i < pollOptions.length; i++)
-                options += `\n( ${emojiCharacters[i+1]} ) ${pollOptions[i]}`; 
+            for (let i = 0; i < pollOptions.length; i++)
+                options += `\n( ${emojiCharacters[i+1]} ) ${pollOptions[i]}`;
             const embed = new Discord.MessageEmbed()
                 .setColor("#8db255")
                 .setDescription(`▬▬▬▬▬▬▬▬▬** « poll » **▬▬▬▬▬▬▬▬▬▬\n\n**poll question »** ${pollQuestion}\n\n**poll options »** ${options}`)
                 .setTimestamp();
             message.channel.send(embed).then(sentEmbed => {
-                for(let i = 0; i < pollOptions.length; i++)
+                for (let i = 0; i < pollOptions.length; i++) {
                     sentEmbed.react(emojiCharacters[i+1]);
+                }
             });
             return;
-        } 
-        
-        else if(command === "uwu"){
+        }
+
+        else if (command === "uwu") {
             message.delete().catch(O_o=>{});
             return message.channel.send(uwuify(args.join(" ")))
-        } 
-        
-        else if(command === "uwuchannel"){
-            if(args[0] !== "-rm") {
+        }
+
+        else if (command === "uwuchannel") {
+            if (args[0] !== "-rm") {
                 fs.writeFile("uwuchannels.txt", `${message.channel.id}\n`, function(err) {
-                    if (err) 
-                        return console.log(err);
-                }); 
+                    if (err) return console.log(err);
+                });
                 return message.channel.send(`new uwuchannel added: ${message.channel.id} (${message.channel.name})`);
-            } 
-        } 
-        
-        else if (command === "data"){
+            }
+        }
+
+        else if (command === "data") {
             var info = `guild: ${message.guild} (\`${message.guild.id}\`)\nchannel: ${message.channel.name} (\`${message.channel.id}\`)\nuser: ${message.author.username} (${message.guild.member(message.author).displayName}) (\`${message.author.id}\`)`
             return message.channel.send(info)
-        } 
+        }
 
-        else if (command === "cc"){
+        else if (command === "cc") {
             return message.channel.send(message.content.length - 4)
         }
         else if (command === "ccd") {
@@ -189,8 +186,8 @@ bot.on('message', async message => {
                 message.delete();
                 message.channel.send(message.content.length - 5)
             }
-            catch (error) { 
-                message.channel.send(`couldn't delete because: ${error}`); 
+            catch (error) {
+                message.channel.send(`couldn't delete because: ${error}`);
             }
             return
         }
@@ -213,7 +210,7 @@ bot.on('message', async message => {
                     }
                 )
                 num = filteredWords.length - 1
-                message.channel.send("bitz beta: " + num)
+                message.channel.send(num)
                 return message.delete();
             }
             catch (error) {
@@ -221,31 +218,9 @@ bot.on('message', async message => {
             }
             return
         }
-
-
-
-        /*
-         * I don't know why inferkit isn't working :(
-        else if(command === "ai"){
-            const inferkit = new InferKit(inferkitToken);
-            const m = await message.channel.send("generating a response...");
-            let result = await inferkit.process(argsAsMessage)//, {length: 150})
-            m.delete().catch(O_o=>{});
-            message.channel.send(result)
-        }
-        */
-        
-        
-        /*
-        else if (message.content.substring(0, prefix.length) === prefix && 
-                isNaN(message.content.substring(1, message.content.length)) && 
-                message.content.substring(message.content.length-1) !== "$" ) {
-            return message.channel.send(`my documentation's at ${prefix}help`);
-        }
-        */
     }
-    
-    if(message.content.match(/^yay$/))
+
+    if (message.content.match(/^yay$/))
         return message.channel.send("yay");
 
     if (message.content.match(/^joe$/))
@@ -253,21 +228,21 @@ bot.on('message', async message => {
 
 
     msg = message.content.toLowerCase()
-    if(msg.includes("mwah"))
+    if (msg.includes("mwah"))
         return message.react("💋");
 
-    if(msg.includes("μωμ"))
+    if (msg.includes("μωμ"))
         return message.channel.send("μωμ")
-        
-    if(msg.includes("uwu") && msg.includes("owo")){
-        if (msg.indexOf("uwu") < msg.indexOf("owo")) 
+
+    if (msg.includes("uwu") && msg.includes("owo")) {
+        if (msg.indexOf("uwu") < msg.indexOf("owo"))
             var uwuWord = "uwu";
-        else 
+        else
             var uwuWord = "owo";
     } else {
-        if (msg.includes("uwu")) 
+        if (msg.includes("uwu"))
             var uwuWord = "uwu";
-        else if (msg.includes("owo")) 
+        else if (msg.includes("owo"))
             var uwuWord = "owo";
     }
 
@@ -276,35 +251,35 @@ bot.on('message', async message => {
     }
 
     fs.readFile('uwuchannels.txt', 'utf8', function(err, uwuchannels) {
-        if (uwuchannels.includes(message.channel.id)){
+        if (uwuchannels.includes(message.channel.id)) {
             message.delete().catch(O_o=>{});
             return message.channel.send(`**${message.author.username}:** ${uwuify(message.content)}`);
         }
         if (err) return console.log(err);
-    }); 
+    });
 
-    msg = message.content.toLowerCase().split(/\s+/g); //msg is message separated into an array by spaces 
-    for (let i = 0; i < msg.length; i++){
-        if (msg[i].includes(uwuWord)){
-            var lastLetters = msg[i].substring(msg[i].indexOf(uwuWord)+3).split(""); 
-            for (let i = 0; i < lastLetters.length; i++){ // owo!s --> !s 
-                if (!punctuationArray.includes(lastLetters[i])){
+    msg = message.content.toLowerCase().split(/\s+/g); //msg is message separated into an array by spaces
+    for (let i = 0; i < msg.length; i++) {
+        if (msg[i].includes(uwuWord)) {
+            var lastLetters = msg[i].substring(msg[i].indexOf(uwuWord)+3).split("");
+            for (let i = 0; i < lastLetters.length; i++) { // owo!s --> !s
+                if (!punctuationArray.includes(lastLetters[i])) {
                     lastLetters.splice(i,1);
                     i--;
-                } 
+                }
             }
-            if (lastLetters.includes("?") || lastLetters.includes("!")){
-                if (msg[i].length > 999) 
+            if (lastLetters.includes("?") || lastLetters.includes("!")) {
+                if (msg[i].length > 999) {
                     return message.channel.send("...okay you win ;-;");
-                else {
+                } else {
                     var num = 0;
-                    for (let i = 0; i < lastLetters.length; i++){ // sanitize and remove all "." or "~"
-                        if (lastLetters[i] == "." || lastLetters[i] == "~"){
+                    for (let i = 0; i < lastLetters.length; i++) { // sanitize and remove all "." or "~"
+                        if (lastLetters[i] == "." || lastLetters[i] == "~") {
                             lastLetters.splice(i,1);
                             i--;
                         }
                     }
-                    return message.channel.send(uwuWord + lastLetters.join("") + lastLetters.join("")); 
+                    return message.channel.send(uwuWord + lastLetters.join("") + lastLetters.join(""));
                 }
             } else {
                 var symbol;
@@ -312,18 +287,19 @@ bot.on('message', async message => {
                 else if (lastLetters.includes(".")) symbol = ".";
                 if (!symbol) return message.channel.send(uwuWord);
                 var num = 0;
-                for (let i = 0; i < lastLetters.length; i++)
-                    if(lastLetters[i] == symbol) num++;
-                return message.channel.send(uwuWord + symbol.repeat(num)); 
+                for (let i = 0; i < lastLetters.length; i++) {
+                    if (lastLetters[i] == symbol) num++;
+                }
+                return message.channel.send(uwuWord + symbol.repeat(num));
             }
-            
+
         }
     }
 });
 
-function uwuify(text){
+function uwuify(text) {
     text.toLowerCase();
-    for(let i = 0; i < text.length; i++){
+    for(let i = 0; i < text.length; i++) {
         if(text.substring(i,i+1) === "r" || text.substring(i,i+1) === "l")
             text = text.substring(0,i) + "w" + text.substring(i+1);
     }
